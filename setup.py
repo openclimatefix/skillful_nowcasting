02df86a7c7291ce2c494f5e13254bb095f383547
Jacob Bieker <jacob@openclimatefix.org>
2021-09-02 13:01:38 +0100

    Start adding setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="skillful_nowcasting",
+    version="0.0.1",
+    packages=["skillful_nowcasting"],
+    url="https://github.com/openclimatefix/skillful_nowcasting",
+    license="MIT License",
+    company="Open Climate Fix Ltd",
+    author="Jacob Bieker",
+    author_email="jacob@openclimatefix.org",
+    description="PyTorch Skillful Nowcasting GAN Implementation",
+)

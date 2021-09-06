f1ab968dd90aa42c17f9d08f81b1988a26badb5e
Jacob Bieker <jacob@openclimatefix.org>
2021-09-06 13:28:57 +0100

    Add auto-publish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="skillful_nowcasting",
-    version="0.0.1",
+    version="0.0.2",
     packages=["nowcasting_gan"],
     url="https://github.com/openclimatefix/skillful_nowcasting",
     license="MIT License",
@@ -10,4 +10,28 @@ setup(
     author="Jacob Bieker",
     author_email="jacob@openclimatefix.org",
     description="PyTorch Skillful Nowcasting GAN Implementation",
+    keywords=[
+        "artificial intelligence",
+        "deep learning",
+        "transformer",
+        "attention mechanism",
+        "metnet",
+        "forecasting",
+        "remote-sensing",
+        "gan"
+    ],
+    install_requires=["einops~=0.3.0",
+                      "numpy~=1.19.5",
+                      "torchvision~=0.10.0",
+                      "antialiased_cnns",
+                      "pytorch_lightning",
+                      "pytorch_msssim",
+                      "torch"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
 )

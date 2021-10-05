2380f399bb4023ef85a7dbe35b93a2732a9641f7
Jacob Bieker <jacob@bieker.tech>
2021-10-05 12:47:23 +0100

    Change test to match DGMR expected inputs

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,11 +4,11 @@ from nowcasting_gan import NowcastingGAN
 
 def test_nowcasting_gan_creation():
     model = NowcastingGAN(
-        forecast_steps=24,
+        forecast_steps=18,
         input_channels=1,
         output_shape=128,
         latent_channels=768,
-        context_channels=768,
+        context_channels=384,
         num_samples=3,
     )
     x = torch.randn((2, 4, 1, 128, 128))
@@ -18,7 +18,7 @@ def test_nowcasting_gan_creation():
     # MetNet creates predictions for the center 1/4th
     assert out.size() == (
         2,
-        24,
+        18,
         1,
         128,
         128,

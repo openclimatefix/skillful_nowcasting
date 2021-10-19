08d36822fb801914eac4ffafcb8ec2173d8c56a0
Jacob Bieker <jacob@bieker.tech>
2021-10-19 09:49:14 +0100

    Add creating RTD documentation

---
+++ import_deepmind_weights.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import tensorflow_hub
+import torch
+from nowcasting_gan import NowcastingGAN
+import os
+import fsspec
+
+module = tensorflow_hub.load("/home/jacob/256x256/")
+print(module)
+print(module.signatures)
+sig_model = module.signatures['default']
+

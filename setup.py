ffceee9b93c62a74304174375f171daa8931eebe
BumpVersion Action <bumpversion@github-actions>
2021-10-26 15:16:48 +0000

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="skillful_nowcasting",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
     url="https://github.com/openclimatefix/skillful_nowcasting",
     license="MIT License",

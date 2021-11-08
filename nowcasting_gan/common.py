0efb9a1e93035b93ab42f210cba800edd00fa2ba
Jacob Bieker <jacob@bieker.tech>
2021-11-08 15:30:16 +0000

    Add UpsampleGBlock, update GBlock to match pseudocode
    Don't have the layer.txt, as it seems to be a copy of latent_cond.txt, but now get how the two GBlocks differ

--- nowcasting_gan/common.py
+++ nowcasting_gan/common.py
@@ -9,8 +9,10 @@ from nowcasting_gan.layers import SelfAttention2d
 
 
 class GBlock(torch.nn.Module):
+    """Residual generator block without upsampling"""
     def __init__(
-        self, input_channels: int = 12, output_channels: int = 12, conv_type: str = "standard"
+        self, input_channels: int = 12, output_channels: int = 12, conv_type: str = "standard",
+            spectral_normalized_eps = 0.0001
     ):
         """
         G Block from Skillful Nowcasting, see https://arxiv.org/pdf/2104.00954.pdf
@@ -20,6 +22,9 @@ class GBlock(torch.nn.Module):
             conv_type: Type of convolution desired, see satflow/models/utils.py for options
         """
         super().__init__()
+        # TODO Add this to the spectrally normalized layers
+        self.spectral_normalized_eps = spectral_normalized_eps
+        self.output_channels = output_channels
         self.bn1 = torch.nn.BatchNorm2d(input_channels)
         self.bn2 = torch.nn.BatchNorm2d(input_channels)
         self.relu = torch.nn.ReLU()
@@ -30,7 +35,6 @@ class GBlock(torch.nn.Module):
             out_channels=output_channels,
             kernel_size=1,
         )
-        self.upsample = torch.nn.Upsample(scale_factor=2, mode="bilinear")
         # Upsample 2D conv
         self.first_conv_3x3 = torch.nn.ConvTranspose2d(
             in_channels=input_channels,
@@ -44,21 +48,84 @@ class GBlock(torch.nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # Branch 1
-        x1 = self.upsample(x)
-        x1 = self.conv_1x1(x1)
+        # Optionally spectrally normalized 1x1 convolution
+        if x.shape[1] != self.output_channels:
+            # TODO Make spectrally normalized, in pseudocode
+            # conv_1x1 = layers.SNConv2D(
+            #           self._output_channels, kernel_size=1, sn_eps=self._sn_eps)
+            #       sc = conv_1x1(inputs)
+            sc = self.conv_1x1(x)
+        else:
+            sc = x
 
-        # Branch 2
         x2 = self.bn1(x)
         x2 = self.relu(x2)
-        x2 = self.first_conv_3x3(
-            x2, output_size=(2 * x.size()[-2], 2 * x.size()[-1])
-        )  # Make sure size is doubled
+        x2 = self.first_conv_3x3(x2)  # Make sure size is doubled
         x2 = self.bn2(x2)
         x2 = self.relu(x2)
         x2 = self.last_conv_3x3(x2)
-        # Sum combine
-        x = x1 + x2
+        # Sum combine, residual connection
+        x = x2 + sc
+        return x
+
+
+class UpsampleGBlock(torch.nn.Module):
+    """Residual generator block with upsampling"""
+    def __init__(
+            self, input_channels: int = 12, output_channels: int = 12, conv_type: str = "standard",
+            spectral_normalized_eps = 0.0001
+            ):
+        """
+        G Block from Skillful Nowcasting, see https://arxiv.org/pdf/2104.00954.pdf
+        Args:
+            input_channels: Number of input channels
+            output_channels: Number of output channels
+            conv_type: Type of convolution desired, see satflow/models/utils.py for options
+        """
+        super().__init__()
+        # TODO Add this to the spectrally normalized layers
+        self.spectral_normalized_eps = spectral_normalized_eps
+        self.output_channels = output_channels
+        self.bn1 = torch.nn.BatchNorm2d(input_channels)
+        self.bn2 = torch.nn.BatchNorm2d(input_channels)
+        self.relu = torch.nn.ReLU()
+        # Upsample in the 1x1
+        conv2d = get_conv_layer(conv_type)
+        self.conv_1x1 = conv2d(
+            in_channels=input_channels,
+            out_channels=output_channels,
+            kernel_size=1,
+            )
+        self.upsample = torch.nn.Upsample(scale_factor=2, mode="nearest")
+        # Upsample 2D conv
+        self.first_conv_3x3 = torch.nn.ConvTranspose2d(
+            in_channels=input_channels,
+            out_channels=input_channels,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            )
+        self.last_conv_3x3 = conv2d(
+            in_channels=input_channels, out_channels=output_channels, kernel_size=3, padding=1
+            )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Spectrally normalized 1x1 convolution
+        sc = self.upsample(x)
+        # TODO Make spectrally normalized, in pseudocode
+        # conv_1x1 = layers.SNConv2D(
+        #           self._output_channels, kernel_size=1, sn_eps=self._sn_eps)
+        #       sc = conv_1x1(inputs)
+        sc = self.conv_1x1(sc)
+
+        x2 = self.bn1(x)
+        x2 = self.relu(x2)
+        x2 = self.first_conv_3x3(x2)  # Make sure size is doubled
+        x2 = self.bn2(x2)
+        x2 = self.relu(x2)
+        x2 = self.last_conv_3x3(x2)
+        # Sum combine, residual connection
+        x = x2 + sc
         return x
 
 
@@ -130,6 +197,8 @@ class DBlock(torch.nn.Module):
 
 
 class LBlock(torch.nn.Module):
+    """Residual block for the Latent Stack."""
+
     def __init__(
         self, input_channels: int = 12, output_channels: int = 12, conv_type: str = "standard"
     ):
@@ -163,6 +232,11 @@ class LBlock(torch.nn.Module):
         )
 
     def forward(self, x) -> torch.Tensor:
+
+        h0 = self.relu(x)
+
+        h1 = self.conv_1x1
+
         x1 = self.conv_1x1(x)
 
         x2 = self.relu(x)

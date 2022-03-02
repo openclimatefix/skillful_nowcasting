99fc36089b003e167eb81c2d1387ad06138ac90a
BumpVersion Action <bumpversion@github-actions>
2022-03-02 08:24:43 +0000

    Bump version: 1.2.14 → 1.2.15

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="dgmr",
-    version="1.2.14",
+    version="1.2.15",
     packages=find_packages(),
     url="https://github.com/openclimatefix/skillful_nowcasting",
     license="MIT License",

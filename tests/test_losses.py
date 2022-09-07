2575b9ce941601215e57d60cf8be20a39039c207
peterdudfield <peter.dudfield@hotmail.com>
2022-09-07 16:45:17 +0100

    add extra tests

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,5 +1,5 @@
 """ Test loss functions"""
-from dgmr.losses import SSIMLoss, MS_SSIMLoss
+from dgmr.losses import SSIMLoss, MS_SSIMLoss, SSIMLossDynamic, tv_loss
 import torch
 
 
@@ -27,3 +27,25 @@ def test_ms_ssim_loss():
 
     loss = MS_SSIMLoss(convert_range=True)
     assert float(loss(x=x, y=y)) != 0
+
+
+def test_ssim_loss_dynamic():
+
+    x = torch.rand((2, 3, 256, 256))
+    y = torch.rand((2, 3, 256, 256))
+    curr_image = torch.rand((2, 3, 256, 256))
+
+    loss = SSIMLossDynamic()
+    assert float(loss(x=x, y=x, curr_image=curr_image)) == 0
+    assert float(loss(x=x, y=y, curr_image=curr_image)) != 0
+
+    loss = SSIMLossDynamic(convert_range=True)
+    assert float(loss(x=x, y=y, curr_image=curr_image)) != 0
+
+
+def test_tv_loss():
+
+    x = torch.ones((2, 3, 256, 256))
+    x[0, 0, 0, 0] = 2.5
+
+    assert float(tv_loss(img=x, tv_weight=2)) == 2 * (1.5**2 + 1.5**2)

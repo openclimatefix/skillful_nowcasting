568992d1aba64f27c24ed328f0cfdee67259e488
Mews <60406199+Mews@users.noreply.github.com>
2024-06-11 23:11:44 +0100

    Add the path to the source code to sys.path in tests
    This is needed so that pytest can actually import dgmr to run the tests

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,4 +1,12 @@
 """ Test loss functions"""
+
+import os
+import sys
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
+
 from dgmr.losses import SSIMLoss, MS_SSIMLoss, SSIMLossDynamic, tv_loss
 import torch
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
+
 import torch
 import torch.nn.functional as F
 from dgmr import (
